db8d58b51e9c5a23a488dab893fa42a7a95997fc
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-17 01:02:31 -0300

    feat: add /details route to check service infos

---
+++ src/routes/index.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+
+import { IDetails } from "../interfaces";
+
+class Routes {
+  public router: Router = Router();
+
+  constructor() {
+    this.setRoutes();
+  }
+
+  private setRoutes(): void {
+    this.router.use("/details", (req: Request, res: Response) => {
+      const details: IDetails = {
+        version: process.env.npm_package_version,
+        contributors: [
+          process.env.npm_package_contributors_0_name,
+          process.env.npm_package_contributors_1_name,
+        ],
+        repository_url: process.env.npm_package_repository_url,
+      };
+
+      return res.status(StatusCodes.OK).json(details);
+    });
+  }
+}
+
+export default new Routes().router;
