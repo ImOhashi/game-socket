18da4dca3f45465dcae517fb722b1ac6b9333963
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-17 00:19:59 -0300

    fix: refactor server file adding http module

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import { Server } from "http";
 import { logger } from "cyber-logger";
 
 import app from "./app";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => logger.info(`Server running on port: ${port}`));
+const http = new Server(app);
+
+http.listen(port, () => logger.info(`Server running on port: ${port}`));
