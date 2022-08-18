bcaf4186bf731c31903d6edeb14550dac3c1f3ed
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-17 22:49:37 -0300

    feat: implement socket with broadcast

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { logger } from "cyber-logger";
 import { Server } from "http";
+import socket, { Socket } from "socket.io";
 
 import app from "./app";
 
@@ -7,4 +8,18 @@ const port = process.env.PORT || 3000;
 
 const http = new Server(app);
 
+const io = new socket.Server(http);
+
 http.listen(port, () => logger.info(`Server running on port: ${port}`));
+
+io.on("connection", (socket: Socket) => {
+  const { session, player_id } = socket.request.headers;
+  const room = session.toString();
+
+  logger.info(`Player ${player_id} connected!`);
+
+  socket.on(room, (msg) => {
+    logger.info(`Player ${player_id}: ${msg}`);
+    socket.broadcast.emit(room, msg);
+  });
+});
