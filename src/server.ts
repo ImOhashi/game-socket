e0951f5736573e00c3336c675c79f518e0cb14c1
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-17 23:18:00 -0300

    fix: set session in socket

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { logger } from "cyber-logger";
 import { Server } from "http";
-import socket, { Socket } from "socket.io";
+import socket from "socket.io";
 
 import app from "./app";
+import Session from "./socket";
 
 const port = process.env.PORT || 3000;
 
@@ -12,14 +13,4 @@ const io = new socket.Server(http);
 
 http.listen(port, () => logger.info(`Server running on port: ${port}`));
 
-io.on("connection", (socket: Socket) => {
-  const { session, player_id } = socket.request.headers;
-  const room = session.toString();
-
-  logger.info(`Player ${player_id} connected!`);
-
-  socket.on(room, (msg) => {
-    logger.info(`Player ${player_id}: ${msg}`);
-    socket.broadcast.emit(room, msg);
-  });
-});
+io.on("connection", Session.init);
