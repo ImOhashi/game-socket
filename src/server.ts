6a9fe37ad00aa0902891f01be4f9b22cfb085840
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-19 01:43:57 -0300

    fix: code smells

--- src/server.ts
+++ src/server.ts
@@ -4,27 +4,32 @@ import socket from "socket.io";
 
 import app from "./app";
 import Session from "./socket";
-import { Socket } from "socket.io";
 
 const port = process.env.PORT || 3000;
 
 const http = new Server(app);
 
-const io = new socket.Server(http);
+const io: socket.Server = new socket.Server(http);
 
 io.use((socket, next) => {
-    const { session, player_id } = socket.request.headers;
-    const room = session.toString();
-    const players = io.of(`/`).adapter.rooms.get(room)
-    if (players && players.size > 1) {
-        logger.info(`Player ${player_id} refused`)
-        next(new Error("Refused"));
-        return
-    } 
-    logger.info(`Player ${player_id} connected`)
-    next();
-    socket.join(room);
+  const { session, player_id } = socket.request.headers;
+  const room = session.toString();
+
+  const players = io.of(`/`).adapter.rooms.get(room);
+
+  if (players && players.size > 1) {
+    logger.info(`Player ${player_id} refused`);
+
+    next(new Error("Refused"));
+
+    return;
+  }
+
+  logger.info(`Player ${player_id} connected`);
+
+  socket.join(room);
+  next();
 });
 
 http.listen(port, () => logger.info(`Server running on port: ${port}`));
-io.on("connection",  Session.init);
+io.on("connection", Session.init);
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,12 +3,15 @@ import { Socket } from "socket.io";
 
 export default class Session {
   public static init(socket: Socket): void {
-    const { session, player_id } = socket.request.headers;
+    const { session, player_id, event } = socket.request.headers;
+
     const room = session.toString();
+    const sessionEvent = event.toString();
+
     socket
-      .on("main", (msg) => {
+      .on(sessionEvent, (msg) => {
         logger.info(`Player ${player_id}: ${msg}`);
-        socket.to(room).emit("main", msg);
+        socket.to(room).emit(sessionEvent, msg);
       })
       .on("disconnect", () => {
         socket.leave(room);
