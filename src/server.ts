c427e8610bb5c1706287cc11071678b0aa6c8bfc
Emanuel Silva <emanuelsa-rv@hotmail.com>
2022-08-19 00:02:56 -0300

    Feat: players count in room

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,17 @@ import socket from "socket.io";
 
 import app from "./app";
 import Session from "./socket";
+import { Socket } from "socket.io";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const http = new Server(app);
 
 const io = new socket.Server(http);
-
+const players_num = (room : string): number =>{
+    return io.of(`/`).adapter.rooms.get(room).size
+}
 http.listen(port, () => logger.info(`Server running on port: ${port}`));
-
-io.on("connection", Session.init);
+io.on("connection",  (socket: Socket) => {
+    Session.init(socket, players_num);
+});
